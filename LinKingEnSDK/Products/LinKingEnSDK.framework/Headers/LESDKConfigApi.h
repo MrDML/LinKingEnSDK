b4de617e2e1b1f87f071d7828bdc6073e972f3f3
MrDML <dml1630@163.com>
2021-04-12 13:24:19 +0800

    update

--- LinKingEnSDK/Products/LinKingEnSDK.framework/Headers/LESDKConfigApi.h
+++ LinKingEnSDK/Products/LinKingEnSDK.framework/Headers/LESDKConfigApi.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 + (void)fetchSDKConfigComplete:(void(^_Nullable)(NSError *_Nullable error))complete;
 + (void)checkUserInfoComplete:(void(^)(NSError *error))complete;
 + (void)fetchSDKConfigAppId:(NSString *)appId complete:(void(^_Nullable)(NSError *_Nullable error))complete;
++ (void)checkUserInfoWithTime:(int)second complete:(void(^_Nullable)(NSDictionary *_Nullableresult,NSError *error))complete;
 @end
 
 NS_ASSUME_NONNULL_END
